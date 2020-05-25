5e9a287b66f97c7cf28fb9188e9f15d82fcec6ca
william <191marquis@gmail.com>
2020-05-24 23:55:13 -0400

    [DEV] Make loot leveling effect only apply to ore resources

--- src/main/scala/com/maybewilliam/leveledloot/LeveledLoot.scala
+++ src/main/scala/com/maybewilliam/leveledloot/LeveledLoot.scala
@@ -4,27 +4,13 @@ It's essentially the main method of the mod.
 */
 package com.maybewilliam.leveledloot
 
-import net.minecraft.init.Blocks
-import net.minecraft.init.Items
-import net.minecraft.item.ItemStack
+import java.util.Random
+import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.common.Mod
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraftforge.fml.common.Mod.EventHandler
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
 import org.apache.logging.log4j.Logger
-import net.minecraftforge.event.LootTableLoadEvent
-import net.minecraft.util.ResourceLocation
-import net.minecraft.world.storage.loot.functions.LootFunction
-import net.minecraft.world.storage.loot.conditions.LootCondition
-import net.minecraft.world.storage.loot.RandomValueRange
-import net.minecraft.world.storage.loot.LootEntryItem
-import net.minecraft.world.storage.loot.functions.SetCount
-import net.minecraft.world.storage.loot.LootPool
-import java.util.Random
-import net.minecraft.world.storage.loot.LootContext
-import net.minecraftforge.common.MinecraftForge
-import net.minecraftforge.fml.common.Mod.Instance
 
 /** Mod object */
 @Mod(modid = References.MODID, name = References.NAME, version = References.VERSION, acceptedMinecraftVersions = References.ACCEPTED, modLanguage = "scala")
--- src/main/scala/com/maybewilliam/leveledloot/LootEventHandler.scala
+++ src/main/scala/com/maybewilliam/leveledloot/LootEventHandler.scala
@@ -3,30 +3,16 @@ This file is for the changing of loot through event handlers
  */
 package com.maybewilliam.leveledloot
 
+import java.util.List
+import java.util.Random
 import net.minecraft.init.Blocks
 import net.minecraft.init.Items
+import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
-import net.minecraftforge.fml.common.Mod
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
-import net.minecraftforge.fml.common.Mod.EventHandler
-import net.minecraftforge.fml.common.event.FMLInitializationEvent
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
-import org.apache.logging.log4j.Logger
-import net.minecraftforge.event.LootTableLoadEvent
-import net.minecraft.util.ResourceLocation
-import net.minecraft.world.storage.loot.functions.LootFunction
-import net.minecraft.world.storage.loot.conditions.LootCondition
-import net.minecraft.world.storage.loot.RandomValueRange
-import net.minecraft.world.storage.loot.LootEntryItem
-import net.minecraft.world.storage.loot.LootTableList
-import net.minecraft.world.storage.loot.functions.SetCount
-import net.minecraft.world.storage.loot.LootPool
-import java.util.Random
-import net.minecraft.world.storage.loot.LootContext
 import net.minecraftforge.common.MinecraftForge
-import net.minecraftforge.fml.common.Mod.Instance
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber
 import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import org.apache.logging.log4j.Logger
 
 /** An event handler for block loot drops
  *
@@ -35,6 +21,68 @@ import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent
 class LootEventHandler
 {
 
+  /** Returns a drop stack if drop is an ore, otherwise null
+   *
+   *  @param event The HarvestDropsEvent
+   *
+   *  @returns The stack to return or null
+   */
+  def getDropStack(event: HarvestDropsEvent): ItemStack = {
+    val num = event.getDrops.size
+    if(num == 0) {
+      return null
+    }
+
+    val stack = event.getDrops.get(0)
+    var item = stack.getItem
+    var mdata = stack.getMetadata
+
+    val coal = item == Items.COAL
+    val iron = item == Item.getItemFromBlock(Blocks.IRON_ORE)
+    val gold = item == Item.getItemFromBlock(Blocks.GOLD_ORE)
+    val diamond = item == Items.DIAMOND
+    val emerald = item == Items.EMERALD
+    val lapis = item == Items.DYE && mdata == 4
+    val redstone = item == Items.REDSTONE
+    val quartz = item == Items.QUARTZ
+
+    var ore = coal || iron || gold || diamond
+    ore = ore || emerald || lapis || redstone || quartz
+
+    if (ore) {
+      if (iron) {
+        item = Items.IRON_INGOT
+      }
+      if (gold) {
+        item = Items.GOLD_INGOT
+      }
+      if (lapis) {
+        return new ItemStack(item, num, 4)
+      }
+      return new ItemStack(item, num)
+    }
+    return null
+  }
+
+  /** Changes harvest drops if block is an ore
+   *
+   *  @param level The block-breaker's level
+   *  @param list The list of drops
+   */
+  def modifyHarvestDrops(level: Int, fortuneLevel: Int, dropStack: ItemStack, list: List[ItemStack]): Unit = {
+    val prob = 2
+    val times = 3
+    if (prob >= 1) {
+      if (list.size >= 1) {
+        list.clear()
+        var i = 0
+        for (i <- 0 to times) {
+          list.add(dropStack.copy())
+        }
+      }
+    }
+  }
+
   /** Changes harvest drops from broken blocks
    *
    *  @param event The harvest drop event
@@ -42,10 +90,15 @@ class LootEventHandler
   @SubscribeEvent
   def onHarvestDrops(event: HarvestDropsEvent): Unit = {
     val harvester = event.getHarvester
-    if (harvester != null) {
-      Debug.chat(harvester.experienceLevel.toString, harvester)
-      val list = event.getDrops
-      list.clear()
+    if (harvester != null && !event.isSilkTouching) {
+      val dropStack = getDropStack(event);
+      if (dropStack != null) {
+        Debug.chat(harvester.experienceLevel.toString, harvester)
+        val fortune = event.getFortuneLevel
+        val list = event.getDrops
+        val level = harvester.experienceLevel
+          modifyHarvestDrops(level, fortune, dropStack, list)
+      }
     }
   }
 }
